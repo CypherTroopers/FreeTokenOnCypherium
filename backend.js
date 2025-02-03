3c32397adcc964409bc657a581f6a1546acba5e0
CypherTroopers <jin360life@gmail.com>
2025-02-04 01:25:02 +0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -8,22 +8,21 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-// index.html を提供
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// ABI を提供
+
 app.get('/abi', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'CRC20.abi'));
 });
 
-// バイトコードを提供
+
 app.get('/bytecode', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'CRC20.bin'));
 });
 
-// サーバー起動
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
